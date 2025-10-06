cf2127d996e50b1d5a6c1743f917831236afc411
Kenji "Brocks" Ibus Tabugadir <tabugadirkenjibrocks@gmail.com>
2025-10-06 21:08:20 +0800

    added ForLoop and Assignment

---
+++ Assignment/Assignment/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: tabug
+ * Date: 06/10/2025
+ * Time: 9:06 pm
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Assignment
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int[] k = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+			int[] d = {1, 2, 3, 4, 5};
+			
+			foreach (int t in k){
+				if (d[4] > t){
+					Console.WriteLine("Low Values: " + t);
+					if (t % 2 == 0){
+						Console.WriteLine("Even: " + t);
+					}
+					else {
+						Console.WriteLine("Odd: " + t);
+					}
+				}
+				else if (d[4] < t){
+					Console.WriteLine("High Values: " + t);
+					if (t % 2 == 0){
+						Console.WriteLine("Even: " + t);
+					}
+					else {
+						Console.WriteLine("Odd: " + t);
+					}
+				}
+			}
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ Loop/ForLoop/ForLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: tabug
+ * Date: 06/10/2025
+ * Time: 9:04 pm
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace ForLoop
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int i, j, rows;
+			
+			Console.Write("Input number of rows: ");
+			rows = Convert.ToInt32(Console.ReadLine());
+			for(i = 1; i <= rows; i++)
+			{
+				for(j = 1; j <= i; j++)
+					Console.Write("*");
+				Console.Write("\n");
+			}
+			Console.ReadKey();
+		}
+	}
+}
